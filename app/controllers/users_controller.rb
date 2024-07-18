0e8f41963fd1fd82970dd026dec1ef0e31392864
tetsuya hasegawa <ttea.oo.cl@gmail.com>
2024-07-18 16:53:47 +0900

    profile画像のデフォ追加自動削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
-
   def show
     @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
+    
+    if params[:remove_avatar] == 'on'
+      @user.avatar.purge if @user.avatar.attached?
+    end
+
     if @user.update(user_params)
       redirect_to @user
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ApplicationRecord
          validates :name, presence: true, uniqueness: { case_sensitive: false }
 
          has_one_attached :avatar
+         attr_accessor :remove_avatar
 end
