c4eba7e769582ff4db1e2ae252cdb150fca713df
tetsuya hasegawa <ttea.oo.cl@gmail.com>
2024-07-21 04:39:52 +0900

    Googleボタンの修正、エラーメッセージ

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,13 +16,13 @@ class User < ApplicationRecord
 
          validates :uid, presence: true, uniqueness: { scope: :provider }, if: -> { uid.present? }
 
-         def self.from_omniauth(auth)
-          where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-            user.name = auth.info.name
-            user.email = auth.info.email
-            user.password = Devise.friendly_token[0, 20]
-          end
-        end
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 
   def self.create_unique_string
     SecureRandom.uuid
