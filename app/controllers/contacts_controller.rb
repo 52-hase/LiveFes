513093908362cc2f4c3e5b22c1af45389a07814f
tetsuya hasegawa <ttea.oo.cl@gmail.com>
2024-08-21 02:28:20 +0900

    お問い合わせフォームの実装アラートの修正2

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,10 +8,12 @@ class ContactsController < ApplicationController
     if @contact.save
       ContactMailer.contact_email(@contact).deliver_now
       flash[:notice] = "お問い合わせが送信されました。"
+      flash.delete(:alert) # エラーメッセージを削除
       redirect_to new_contact_path
     else
       flash[:alert] = "お問い合わせの送信に失敗しました。"
       render :new
+      flash.delete(:alert) # エラーメッセージを削除
     end
   end
 
