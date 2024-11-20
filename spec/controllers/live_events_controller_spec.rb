8b66e14dd682bc69f729c6a828dee17a0b558519
tetsuya hasegawa <ttea.oo.cl@gmail.com>
2024-11-21 02:30:57 +0900

    ライブイベントのテストについて

--- spec/controllers/live_events_controller_spec.rb
+++ spec/controllers/live_events_controller_spec.rb
@@ -3,17 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe LiveEventsController, type: :controller do
+  # テスト対象のクラスとして LiveEventsController を指定し、RSpec でコントローラのテストを行う
+
   describe "GET #index" do
+    # index アクションのテストに関する記述
+
     it "returns a successful response" do
+      # index アクションをリクエストした際に、レスポンスが成功 (200 OK) を返すことを確認する
       get :index
       expect(response).to be_successful
     end
   end
 
   describe "GET #show" do
+    # show アクションのテストに関する記述
+
     it "returns a successful response" do
+      # テスト用の LiveEvent レコードを作成
       live_event = LiveEvent.create!(title: "Test Event")
+
+      # 作成したレコードの ID を使って show アクションをリクエスト
       get :show, params: { id: live_event.id }
+
+      # show アクションのレスポンスが成功 (200 OK) を返すことを確認する
       expect(response).to be_successful
     end
   end
