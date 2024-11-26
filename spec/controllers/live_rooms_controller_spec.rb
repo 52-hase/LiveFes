7e737c9a95860acb8059a25be21f8c33d225db16
tetsuya hasegawa <ttea.oo.cl@gmail.com>
2024-11-27 00:28:39 +0900

    リアルタイムチャット機能のテスト記載修正

--- spec/controllers/live_rooms_controller_spec.rb
+++ spec/controllers/live_rooms_controller_spec.rb
@@ -5,17 +5,21 @@ require 'rails_helper'
 RSpec.describe LiveRoomsController, type: :controller do
   describe 'POST #create' do
     context 'with valid params' do
+
+      # 新しいライブルームが正常に作成されること
       it 'creates a new live room' do
         expect {
           post :create, params: { live_room: { name: 'New Live Room' } }
         }.to change(LiveRoom, :count).by(1)  # 新しいルームが1つ増えることを確認
       end
 
+      # 新しく作成したライブルームの詳細ページにリダイレクトされること
       it 'redirects to the new live room' do
         post :create, params: { live_room: { name: 'New Live Room' } }
         expect(response).to redirect_to(LiveRoom.last)  # 作成したルームの詳細ページにリダイレクトされることを確認
       end
 
+      # フラッシュメッセージが正しく設定されること
       it 'sets a flash notice' do
         post :create, params: { live_room: { name: 'New Live Room' } }
         expect(flash[:notice]).to eq('ルームが作成されました。')  # フラッシュメッセージの確認
@@ -23,6 +27,8 @@ RSpec.describe LiveRoomsController, type: :controller do
     end
 
     context 'with invalid params' do
+
+      # 無効なパラメータでルーム作成を試みた際にルームが作成されないことをテスト
       it 'does not create a live room' do
         expect {
           post :create, params: { live_room: { name: '' } }  # 無効な名前を送信
